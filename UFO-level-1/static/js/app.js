b371bb609c0db5c1857d0beadafdbd0f372120a8
Ed Yancik <68304939+Yanwho@users.noreply.github.com>
2021-01-05 15:53:25 -0700

    coded data table display

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,1 +1,30 @@
+function showData(data) {
+    
+    let table = d3.select("#data-table")
+
+    table.selectAll("tr").remove()
+    
+    let headerRow = table.append("tr")
+
+    headerRow.append("th").text("Date")
+    headerRow.append("th").text("City")
+    headerRow.append("th").text("State")
+    headerRow.append("th").text("Country")
+    headerRow.append("th").text("Shape")
+    headerRow.append("th").text("Duration")
+    headerRow.append("th").text("Comments")
+
+    let tableBody = table.append("tbody")
+
+    data.forEach((d) => {
+        let row = tableBody.append("tr")
+        row.append("td").text(d.datetime)
+        row.append("td").text(d.city)
+        row.append("td").text(d.state)
+        row.append("td").text(d.country)
+        row.append("td").text(d.shape)
+        row.append("td").text(d.durationMinutes)
+        row.append("td").text(d.comments)
+    })
+}
 
